bc42cc6a52284e6a5599d2c4aacc36c94ea829ea
qork13 <qork13@gmail.com>
2021-11-16 17:31:50 -0600

    Committing shell controllers
    Pushing shell linked list controller and priority queue controller
    Updated GUI

--- src/MediaGUI.java
+++ src/MediaGUI.java
@@ -17,33 +17,29 @@ public class MediaGUI {
 
 	        //Creating the MenuBar and adding components
 	        JMenuBar mb = new JMenuBar();
-	        JMenu m1 = new JMenu("FILE");
+	        JMenu m1 = new JMenu("File");
 	        JMenu m2 = new JMenu("Help");
 	        mb.add(m1);
 	        mb.add(m2);
-	        JMenuItem m11 = new JMenuItem("Open");
-	        JMenuItem m22 = new JMenuItem("Save as");
+	        JMenuItem m11 = new JMenuItem("New");
 	        m1.add(m11);
-	        m1.add(m22);
+	        
 
 	        //Creating the panel at bottom and adding components
-	        JPanel panel = new JPanel(); // the panel is not visible in output
-	        JLabel label = new JLabel("Enter Text");
-	        JTextField tf = new JTextField(10); // accepts upto 10 characters
-	        JButton send = new JButton("Send");
+	        JPanel panel = new JPanel(); 
+	        JLabel label = new JLabel("Title");
+	        JTextField title = new JTextField(30); 
+	        JButton submit = new JButton("Submit");
 	        JButton reset = new JButton("Reset");
 	        panel.add(label); // Components Added using Flow Layout
-	        panel.add(tf);
-	        panel.add(send);
+	        panel.add(title);
+	        panel.add(submit);
 	        panel.add(reset);
 
-	        // Text Area at the Center
-	        JTextArea ta = new JTextArea();
 
 	        //Adding Components to the frame.
-	        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+	        frame.getContentPane().add(BorderLayout.CENTER, panel);
 	        frame.getContentPane().add(BorderLayout.NORTH, mb);
-	        frame.getContentPane().add(BorderLayout.CENTER, ta);
 	        frame.setVisible(true);
 	    }
 	 }
